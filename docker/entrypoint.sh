686a8bd8ea1d45328e7b70af044ef0eb5f1b2a25
Sven Neumann <sven.neumann.ext@aboutyou.com>
2022-02-06 00:22:10 +0100

    do not use supervisor, go for direct start instead.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
-supervisorctl reread
-supervisorctl update
-supervisorctl start nlpserver 
+python3 /usr/src/nlpserver/nlpserver.py
